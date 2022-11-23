013b285e3ae55f3d94d874cb1d56e2f8283c5ba7
Ahmedkhaledhassan <ahmedkhaledhasan40@gmail.com>
2022-11-23 13:57:26 -0800

    stsrting checkout screen

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:e_commerce2/model/item.dart';
+import 'package:e_commerce2/shared/appbar.dart';
 import 'package:e_commerce2/shared/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -19,40 +20,7 @@ class _DetailsState extends State<Details> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    bottom: 22,
-                    child: Container(
-                        child: Text(
-                          "0",
-                          style: TextStyle(
-                              fontSize: 12,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(210, 95, 143, 187),
-                            shape: BoxShape.circle)),
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.add_shopping_cart_sharp)),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 11),
-                child: Text(
-                  "\$10",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
-          ),
-        ],
+        actions: [productsAndPrice()],
         backgroundColor: appbarblue,
         title: Text("Details screen"),
       ),
