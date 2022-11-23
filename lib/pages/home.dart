6c258f333bd155bdde0bb4c6a185f10b4fde8c3c
Ahmedkhaledhassan <ahmedkhaledhasan40@gmail.com>
2022-11-23 05:24:58 -0800

    make ListTile

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,26 +13,51 @@ class Home extends StatelessWidget {
         drawer: Drawer(
             child: Column(
           children: [
-            UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://tse1.mm.bing.net/th?id=OIP.oiWY-o1RpBabm0fzFow9TwHaEQ&pid=Api&P=0"),
-                      fit: BoxFit.cover),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://tse1.mm.bing.net/th?id=OIP.oiWY-o1RpBabm0fzFow9TwHaEQ&pid=Api&P=0"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: NetworkImage(
+                            "https://collectionimages.npg.org.uk/large/mw71457/David-Bomberg-Self-Portrait-with-Pipe.jpg")),
+                    accountName: Text("Ahmed Khaled ",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        )),
+                    accountEmail: Text("ahmedkhaledhasa40@gmail.com")),
+                ListTile(
+                  title: Text("Home"),
+                  leading: Icon(Icons.home),
+                  onTap: () {},
                 ),
-                currentAccountPicture: CircleAvatar(
-                    radius: 55,
-                    backgroundImage: NetworkImage(
-                        "https://collectionimages.npg.org.uk/large/mw71457/David-Bomberg-Self-Portrait-with-Pipe.jpg")),
-                accountName: Text("Ahmed Khaled ",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    )),
-                accountEmail: Text("ahmedkhaledhasa40@gmail.com")),
-            ListTile(
-              title: Text("Products"),
-              leading: Icon(Icons.add_shopping_cart_sharp),
-              onTap: () {},
+                ListTile(
+                  title: Text("Products"),
+                  leading: Icon(Icons.add_shopping_cart_sharp),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text("About"),
+                  leading: Icon(Icons.help_center),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text("Logout"),
+                  leading: Icon(Icons.logout_rounded),
+                  onTap: () {},
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text("Developed by Ahmed Khaled © 2022",
+                  style: TextStyle(fontSize: 16)),
             )
           ],
         )),
